743866b1f1610c16e9e3f152181e8c5d93c4d861
tomdid <totas.ss@gmail.com>
2020-06-21 23:47:30 +0100

    removed comments

--- server/server.ts
+++ server/server.ts
@@ -2,13 +2,6 @@ import { createServer } from 'http';
 import next from 'next';
 import { parse } from 'url';
 
-// require('react-dom');
-// // @ts-ignore
-// global.RRR2 = require('react');
-// console.log('HEEEERE');
-// // @ts-ignore
-// console.log(global.RRR === global.RRR2);
-
 const port = parseInt(String(process.env.PORT || '3000'), 10);
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
